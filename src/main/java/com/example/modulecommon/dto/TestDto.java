b6bdf42aee14b06e8d4e17ea6fbe4f4affa0ffaf
EunDuk2 <dmsqls6789@naver.com>
2025-10-01 18:50:12 +0900

    publish test

---
+++ src/main/java/com/example/modulecommon/dto/TestDto.java
@@ -0,0 +1,14 @@
+package com.example.modulecommon.dto;
+
+public class TestDto {
+    private String name;
+    private int age;
+
+    public TestDto(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() { return name; }
+    public int getAge() { return age; }
+}
