261677d73e4b4d1b3af6cdb905ba5e9c24e4e4c1
EunDuk2 <dmsqls6789@naver.com>
2025-10-02 20:57:47 +0900

    [1.0.8] - common/domain - BaseEntity 추가

---
+++ src/main/java/com/example/modulecommon/domain/BaseTimeEntity.java
@@ -0,0 +1,20 @@
+package com.example.modulecommon.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+public class BaseTimeEntity {
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
